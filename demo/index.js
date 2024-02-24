79e4285eeacde4e73560fc7bfba2a5e15b863c86
Nightre <nightscratchs@gmail.com>
2024-02-24 15:52:12 +0800

    Update index.js

--- demo/index.js
+++ demo/index.js
@@ -36,49 +36,6 @@ rapid.canvas.onmousedown = () => {
 rapid.canvas.onmouseup = () => {
     add = false
 }
-//                    R    G  B  A
-const red = new Color(255, 0, 0, 255)
-const blue = new Color(0, 0, 255, 255)
-const yellow = new Color(255, 255, 0, 255)
-const green = new Color(0, 255, 0, 255)
-
-const drawGraphicDemo = () => {
-    rapid.startGraphicDraw()
-    const s = Math.sin(time)
-    rapid.addGraphicVertex(50 + s * 50, 100, red)
-    rapid.addGraphicVertex(200, 50, blue)
-    rapid.addGraphicVertex(200 + s * 100, 200, yellow)
-    rapid.addGraphicVertex(100, 300 + s * 100, green)
-    rapid.addGraphicVertex(50, 300, green)
-    rapid.endGraphicDraw()
-}
-
-const drawMatrixStackDemo = () => {
-    const s = Math.sin(time)
-    rapid.matrixStack.translate(150, 50)
-    rapid.save() // save matrixStack
-    rapid.matrixStack.rotate(s * 0.5 + 0.1)
-    rapid.renderSprite(cat, 0, 0, yellow)
-    rapid.matrixStack.translate(32, 32)
-    rapid.renderSprite(cat, 0, 0, green)
-    rapid.matrixStack.translate(32, 32)
-    rapid.save() // save matrixStack
-    rapid.renderSprite(plane)
-
-    rapid.matrixStack.translate(32, 32)
-    rapid.matrixStack.rotate(s)
-    rapid.matrixStack.scale(2 + 1 * s)
-
-    rapid.renderSprite(cat)
-    rapid.matrixStack.translate(32, 32)
-    rapid.renderSprite(plane)
-    rapid.restore()
-    rapid.matrixStack.translate(50, 50)
-    rapid.renderSprite(plane)
-    rapid.restore()
-    rapid.matrixStack.translate(50, 50)
-    rapid.renderSprite(cat,0,0,red)
-}
 
 const render = () => {
     if (add) {
@@ -106,16 +63,7 @@ const render = () => {
         rapid.matrixStack.translate(element.x, element.y)
         rapid.renderSprite(element.texture)
         rapid.restore()
-
-        // if (index == 3000) {
-        //     drawGraphicDemo()
-        // }
     }
-    // if (sprites.length < 3000) {
-    //     drawGraphicDemo()
-    // }
-    // drawMatrixStackDemo()
-
     rapid.endRender()
 }
 // performance monitor
